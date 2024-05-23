5ae568dbb79797c0a2023ce9859fdc4292ffd80a
Alan Bersia <alanbersia@gmail.com>
2024-05-23 15:01:00 -0300

    Subida inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,31 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    // {
+    //   provide: HTTP_INTERCEPTORS,
+    //   useClass: HttpRequestInterceptor,
+    //   multi: true
+    // },
+    provideAnimationsAsync(),
+    // importProvidersFrom(
+
+    //   JwtModule.forRoot({
+    //     config: {
+    //       tokenGetter: () => {
+    //         return localStorage.getItem(`tkn_${environment.app}`)
+    //       },
+    //       // allowedDomains: ['localhost:4200'],
+    //     },
+    //   }),
+    // ),
+    // provideHttpClient(withInterceptorsFromDi()),
+  ]
 };
